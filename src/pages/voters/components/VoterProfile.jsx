d3cb8da914f8d5dff659fb1d3857f7ecd14d2dc6
John Rhay <javier.johnrhay@gmail.com>
2025-04-21 21:23:08 +0800

    fix: change proxy assignment form disable condition

--- src/pages/voters/components/VoterProfile.jsx
+++ src/pages/voters/components/VoterProfile.jsx
@@ -372,7 +372,7 @@ const VoterProfile = ({ voters, votersLoading }) => {
                 </div>
             </div>
 
-            <div className={`w-[95%] bg-white rounded-md shadow-[0_0_20px_rgba(0,0,0,0.1)] p-5 flex flex-col gap-4 ${!user?.allow_proxy || user?.proxy_id !== null || myProxiedUsers ? "pointer-events-none opacity-20" : ""}`}>
+            <div className={`w-[95%] bg-white rounded-md shadow-[0_0_20px_rgba(0,0,0,0.1)] p-5 flex flex-col gap-4 ${!user?.allow_proxy || user?.proxy_id !== null || myProxiedUsers?.length !== 0 ? "pointer-events-none opacity-20" : ""}`}>
                 <div>
                     <h1 className='font-semibold text-2xl'>I Want to Appoint a Proxy</h1>
                 </div>
